3c9bdf1babfae6b3c3daa5d64e22441d470f41d2
miha2158 <miha.vanya@gmail.com>
2017-08-29 23:14:09 +0500

    Ничего не изменилось

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using static System.Console;
 
 namespace Task10
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-
 
         }
     }
